e28a820315676fac48fb7fa7dc63c8df96426cc2
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-02-17 11:23:46 -0500

    test: add 'path' module to test-find-python.js
    Needed to contruct a path string for a 'new RegExp()'

--- test/test-find-python.js
+++ test/test-find-python.js
@@ -6,6 +6,7 @@ const test = require('tap').test
 const findPython = require('../lib/find-python')
 const execFile = require('child_process').execFile
 const PythonFinder = findPython.test.PythonFinder
+const path = require('path')
 
 require('npmlog').level = 'warn'
 
